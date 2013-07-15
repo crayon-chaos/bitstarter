be47fcbcb402f7259508301024fc6a14791c4b55
crayon_chaos <aarondankert@gmail.com>
2013-07-15 22:28:50 +0000

    working on web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+var buf = new Buffer(fs.readFileSync('index.html'));
+//stringb bu = fs.readFileSync('index.html');
+var string = buf.toString()
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(string);
 });
 
 var port = process.env.PORT || 5000;
